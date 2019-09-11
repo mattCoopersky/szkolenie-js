9909248926db8374360554d6098e7fd09d320ac9
Mateusz Bednarczyk <mateusz.bednarczyk1995@gmail.com>
2019-09-11 16:21:54 +0200

    class with prototype

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+function Pracownik(imie) {
+    this.imie = imie;
+}
+
+Pracownik.prototype.przedstawSie = function() {
+    console.log(`Nazywam sie ${this.imie}`);
+}
+
+var p = new Pracownik("Marcin");
+
+var temp = Object.create(Pracownik.prototype);
+Pracownik.call(temp, "Mateusz");
+var pp = temp;
+
+pp.przedstawSie();
